8ece65373e913970468e25dd7627a4826cfb431d
Mohamed Abdelnasser <peskohere@gmail.com>
2026-07-27 19:43:57 +0300

    lazy loading added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/pages/home/home';
-import { ServicesComponent } from './components/pages/services-component/services-component';
-import { ContactComponent } from './components/pages/contact/contact';
-import { AuthComponent } from './components/auth/auth.component';
-import { authRoutes } from './components/auth/authRoutes';
 
 export const routes: Routes = [
     { path: "", redirectTo: "home", pathMatch: "full" },
     { path: "home", component: Home },
-    { path: "services", component: ServicesComponent },
-    { path: "contact-us", component: ContactComponent },
-    { path: "auth", component: AuthComponent, children: authRoutes },
+    { path: "services", loadComponent: () => import("./components/pages/services-component/services-component").then((c) => c.ServicesComponent) },
+    { path: "contact-us", loadComponent: () => import("./components/pages/contact/contact").then((c) => c.ContactComponent) },
+    { path: "auth", loadComponent: () => import("./components/auth/auth.component").then((c) => c.AuthComponent), loadChildren: () => import("./components/auth/authRoutes").then((m) => m.authRoutes) },
 ];
