baf4705301ace8ffa84df01800574a8b78f22742
Aya_Ramadan <ramadanaya442@gmail.com>
2026-07-28 18:13:55 -0700

    Fix contact support routing

--- src/app/components/pages/services-component/services-component.ts
+++ src/app/components/pages/services-component/services-component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-services',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './services-component.html',
   styleUrl: './services-component.css',
 })
