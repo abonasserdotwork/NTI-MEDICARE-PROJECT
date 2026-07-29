c1aed9bdfd30dd41e38f16f687483c750f271b51
Mohamed Abdelnasser <peskohere@gmail.com>
2026-07-30 01:29:53 +0300

    Log out and Delete Account Functionality

--- src/app/components/pages/dashboard/components/settings/settings.ts
+++ src/app/components/pages/dashboard/components/settings/settings.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../../../services/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
@@ -61,9 +63,9 @@ export class SettingsComponent implements OnInit {
     }
   };
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   get latestNotifications() {
     return this.notificationsList.slice(-2);
@@ -77,10 +79,42 @@ export class SettingsComponent implements OnInit {
     console.log('Updated Settings:', this.settings);
   }
 
+  terminateSession() {
+    if (localStorage.getItem('user')) {
+      localStorage.removeItem('user');
+      this.router.navigate(['/home']);
+
+      return 1;
+    } else if (sessionStorage.getItem('user')) {
+      sessionStorage.removeItem('user');
+      this.router.navigate(['/home']);
+
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  clearMemoryStorage(userId: number) {
+    const deletedItems = [`categories_${userId}`, `emergencyContacts_${userId}`, `history_${userId}`, `medicines_${userId}`];
+    for (let item of deletedItems) {
+      if (localStorage.getItem(item)) {
+        localStorage.removeItem(item)
+      }
+    }
+  }
+
   confirmDeleteAccount() {
     const isConfirmed = confirm('Are you sure you want to delete your account? This action cannot be undone.');
     if (isConfirmed) {
-      alert('Account deleted successfully.');
+      const user = this.userService.getCurrentUser();
+      const users = this.userService.getUsers();
+      if (user) {
+        const userIndex = users.findIndex((u) => u.id === user.id);
+        users.splice(userIndex, 1);
+        this.clearMemoryStorage(user.id);
+        this.terminateSession();
+      }
     }
   }
 
